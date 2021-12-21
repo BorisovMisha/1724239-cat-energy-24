7770be7997ca7520e99a0ed85513336c2b610796
BorisovMisha <bor_mb@mail.ru>
2021-12-21 21:08:34 +0300

    Работа с декоративными элементами

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -1,7 +1,14 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.page-header__toggle');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-navToggle.addEventListener('click', function() {
-  navMain.classList.toggle('main-nav--closed');
-  navToggle.classList.toggle('page-header__toggle--close');
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
 });

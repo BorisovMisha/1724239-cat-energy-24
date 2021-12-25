67165af36e1ef03b5edc3c530de48f48d27d46af
BorisovMisha <bor_mb@mail.ru>
2021-12-25 18:54:12 +0300

    Меню на планшете и десктопе

--- source/js/mobile-menu-1.js
+++
@@ -1,7 +0,0 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.page-header__toggle');
-
-navToggle.addEventListener('click', function() {
-  navMain.classList.toggle('main-nav--closed');
-  navToggle.classList.toggle('page-header__toggle--close');
-});
